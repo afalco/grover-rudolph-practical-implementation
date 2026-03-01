07ed0ab25897816e4c185efa1b96aa6a92795f22
Antonio Falco <Antonio.Falco@gmail.com>
2026-03-01 12:01:15 +0100

    Refactor imports and update __all__ in __init__.py

--- gr/__init__.py
+++ gr/__init__.py
@@ -1,19 +1,46 @@
 # gr/__init__.py
+from __future__ import annotations
 
-from .angles import normalize_prob8, angles_3q_asin_child1
-from .circuit import build_gr_circuit_3q
-from .metrics import tv_l2_fidelity, per_qubit_marginals
-from .backends import run_sim_probs, run_nmr_repeated_avg
-from .readout import calibrate_readout_matrix_8x8, mitigate_readout
+from .constants import STATES_3Q, NQ_3Q
+from .angles import normalize_prob8, target_from_prob8, angles_3q_asin_child1
+from .circuit import build_gr_circuit_3q, add_identity_safe_tail
+from .metrics import (
+    normalize_dict,
+    tv_l2_fidelity,
+    per_qubit_marginals,
+    print_distribution,
+    print_marginals,
+    print_compare,
+)
+from .backends import (
+    run_sim_probs,
+    run_nmr_probs_robust,
+    run_nmr_repeated_avg,
+    load_nmr_credentials_from_env,
+)
+from .readout import (
+    calibrate_readout_matrix_8x8,
+    mitigate_readout,
+)
 
 __all__ = [
+    "STATES_3Q",
+    "NQ_3Q",
     "normalize_prob8",
+    "target_from_prob8",
     "angles_3q_asin_child1",
     "build_gr_circuit_3q",
+    "add_identity_safe_tail",
+    "normalize_dict",
     "tv_l2_fidelity",
     "per_qubit_marginals",
+    "print_distribution",
+    "print_marginals",
+    "print_compare",
     "run_sim_probs",
+    "run_nmr_probs_robust",
     "run_nmr_repeated_avg",
+    "load_nmr_credentials_from_env",
     "calibrate_readout_matrix_8x8",
     "mitigate_readout",
 ]
